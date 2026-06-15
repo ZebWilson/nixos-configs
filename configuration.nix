0565685c5adbca88af5ecd84410a368d63ae82c4
Zeb <33135248+ZebWilson@users.noreply.github.com>
2026-06-15 11:11:06 +0530

    first commit

---
+++ configuration.nix
@@ -0,0 +1,331 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{
+  config,
+  pkgs,
+  unstable,
+  antigravity-nix,
+  ...
+}:
+
+{
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
+  services.flatpak.enable = true;
+  services.flatpak.packages = [
+    "com.stremio.Stremio"
+  ];
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  # For saving QLC disks from too much write during builds - use RAM for build
+  boot.tmp.useTmpfs = true;
+  # Optional: limit tmpfs size if you are tight on RAM (defaults to 50% of RAM)
+  boot.tmp.tmpfsSize = "50%";
+
+  # Saving disk spill for cache by compression RAM Cache
+  zramSwap = {
+    enable = true;
+    priority = 100; # Higher priority - to avoid swap usage on ssd
+    memoryPercent = 50; # Allocates up to 50% of your total RAM to the compressed swap device
+    algorithm = "zstd"; # Excellent compression ratio and speed
+  };
+
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  networking.hostName = "laptop"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+  networking.networkmanager.wifi.powersave = false;
+
+  # Set your time zone.
+  time.timeZone = "Asia/Kolkata";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_IN";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_IN";
+    LC_IDENTIFICATION = "en_IN";
+    LC_MEASUREMENT = "en_IN";
+    LC_MONETARY = "en_IN";
+    LC_NAME = "en_IN";
+    LC_NUMERIC = "en_IN";
+    LC_PAPER = "en_IN";
+    LC_TELEPHONE = "en_IN";
+    LC_TIME = "en_IN";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+    # of just the bare essentials.
+    powerManagement.enable = true;
+
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = true;
+    prime.offload.enable = true;
+    prime.intelBusId = "PCI:0:2:0";
+    prime.nvidiaBusId = "PCI:1:0:0";
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of
+    # supported GPUs is at:
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+    # Only available from driver 515.43.04+
+    # Currently "stable" 580 usually refers to the proprietary one.
+    open = true;
+
+    # Enable the Nvidia settings menu,
+    # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  # Enable the GNOME Desktop Environment.
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  ##fonts
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+    (fetchzip {
+      url = "https://github.com/googlefonts/literata/archive/refs/heads/main.zip";
+      hash = "sha256-IZlPLvRbHb/tDOKPL6Veh83hTrbuOD59FXNrfYmVK5E="; # The SHA-256 hash of the zip file
+      stripRoot = false;
+    })
+  ];
+
+  fonts.fontconfig = {
+    enable = true;
+    defaultFonts = {
+      monospace = [ "JetBrainsMono Nerd Font" ];
+      serif = [ "Literata" ];
+      # sansSerif = [ "Some Sans Font" ];
+    };
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.zeb = {
+    isNormalUser = true;
+    description = "Zeb";
+    hashedPasswordFile = config.sops.secrets.user-password.path;
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+    packages = with pkgs; [
+      gnome-tweaks
+      qbittorrent
+      python314
+      uv
+      unstable.blender
+      unstable.kicad
+      unstable.freecad
+      unstable.ghostty
+      unstable.google-chrome
+      antigravity-nix.packages.${pkgs.stdenv.hostPlatform.system}.google-antigravity-ide-no-fhs
+      antigravity-nix.packages.${pkgs.stdenv.hostPlatform.system}.google-antigravity-cli
+    ];
+  };
+
+  # Install firefox.
+  programs.firefox.enable = true;
+  programs.xwayland.enable = true;
+  programs.nix-ld.enable = true;
+  programs.starship.enable = true;
+
+  programs.zsh = {
+    enable = true;
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
+  };
+
+  users.users.zeb.shell = pkgs.zsh;
+
+  programs.nix-ld.libraries = with pkgs; [
+    stdenv.cc.cc.lib
+    zlib
+    glib
+    # You can even put CUDA here if you want it globally available to pip/uv!
+    cudatoolkit
+    config.boot.kernelPackages.nvidia_x11
+  ];
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    # version control
+    git
+
+    # networking
+    wget
+    curl
+
+    # Core tools
+    helix
+    pciutils
+    usbutils
+    htop
+    unzip
+    zellij
+
+    # nix setup
+    nixfmt
+    nix-direnv
+    direnv
+    nixd
+
+    # Password management
+    age
+    sops
+  ];
+
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+  };
+
+  nix.settings.auto-optimise-store = false;
+  nix.optimise = {
+    automatic = true;
+    dates = [ "weekly" ];
+  };
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
+
+  ## keybindings
+  programs.dconf.profiles.user.databases = [
+    {
+      settings = {
+        # Window cycling configurations
+        "org/gnome/desktop/wm/keybindings" = {
+          switch-windows = [ "<Alt>Tab" ];
+          switch-windows-backward = [ "<Shift><Alt>Tab" ];
+
+          switch-applications = pkgs.lib.gvariant.mkEmptyArray pkgs.lib.gvariant.type.string;
+          switch-applications-backward = pkgs.lib.gvariant.mkEmptyArray pkgs.lib.gvariant.type.string;
+        };
+
+        # Define the list of custom keybindings paths
+        "org/gnome/settings-daemon/plugins/media-keys" = {
+          custom-keybindings = [
+            "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+          ];
+        };
+
+        # Configure custom0 to run Ghostty on Super+T
+        "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+          name = "Open Ghostty";
+          command = "ghostty";
+          binding = "<Super>t";
+        };
+      };
+    }
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.11"; # Did you read the comment?
+
+  sops = {
+    defaultSopsFile = ./secrets.yaml;
+    defaultSopsFormat = "yaml";
+    age = {
+      keyFile = null;
+      sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+    };
+    secrets = {
+      user-password = {
+        neededForUsers = true;
+      };
+    };
+  };
+
+}
---
+++ flake.nix
@@ -0,0 +1,69 @@
+{
+  description = "An NixOS flake template that you can adapt to your own system";
+
+  # Flake inputs
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0"; # Stable Nixpkgs (use 0.1 for unstable)
+    determinate = {
+      url = "https://flakehub.com/f/DeterminateSystems/determinate/3"; # Determinate 3.*
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-flatpak.url = "github:gmodena/nix-flatpak";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    antigravity-nix.url = "github:jacopone/antigravity-nix";
+  };
+
+  # Flake outputs
+  outputs =
+    { self, ... }@inputs:
+    let
+      # Change this if you're building for a system type other than x86 AMD Linux
+      system = "x86_64-linux";
+
+      # The flake output name of your system (`nixosConfigurations.${key}`). Change this
+      # to make it less generic
+    in
+    {
+      # A minimal (but updatable!) NixOS configuration output by this flake
+      nixosConfigurations.laptop = inputs.nixpkgs.lib.nixosSystem {
+        inherit system;
+
+        # NixOS modules
+        modules = [
+          # Load the Determinate module, which provides Determinate Nix
+          inputs.determinate.nixosModules.default
+          # Load the Flatpak module
+          inputs.nix-flatpak.nixosModules.nix-flatpak
+          # Load sops-nix for setting up age + sops
+          inputs.sops-nix.nixosModules.sops
+          # Load the hardware configuration from a separate file (a common convention for NixOS)
+          ./configuration.nix
+          # This module provides a minimum viable NixOS configuration
+
+        ];
+
+        specialArgs = {
+          # Values to pass to modules
+          unstable = import inputs.nixpkgs-unstable {
+            inherit system;
+            config.allowUnfree = true;
+          };
+          antigravity-nix = inputs.antigravity-nix;
+        };
+      };
+
+      # Nix formatter
+
+      # This applies the formatter that follows RFC 166, which defines a standard format:
+      # https://github.com/NixOS/rfcs/pull/166
+
+      # To format all Nix files:
+      # git ls-files -z '*.nix' | xargs -0 -r nix fmt
+      # To check formatting:
+      # git ls-files -z '*.nix' | xargs -0 -r nix develop --command nixfmt --check
+    };
+}
